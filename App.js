66cd115f33bbc02928aab730c42c75ae17c14206
quangChe <quang.n.che@gmail.com>
2019-03-08 13:29:05 -0800

    CommentList Component
    Added a CommentList component that displays list of comments associated with every image card item. This component will be rendered in the Comment Screen

--- App.js
+++ App.js
@@ -3,19 +3,21 @@ import { StyleSheet, View, Platform } from 'react-native';
 import { Constants } from 'expo';
 
 import FeedScreen from './screens/FeedScreen';
+import CommentList from './components/CommentList';
 
 export default class App extends React.Component {
   state = {
-    items: [
-      {id: 11, author: 'Quang Che'},
-      {id: 12, author: 'Robert Nguyen'},
-      {id: 13, author: 'Forest Whitaker'}
+    comments: [
+      'Wow', 'This is great!', 'Beautiful image!'
     ]
   }
   render() {
     return (
       <View style={styles.container}>
-        <FeedScreen style={styles.feed}></FeedScreen>
+        {/* <FeedScreen style={styles.feed}/> */}
+        <CommentList
+          items={this.state.comments}
+        />
       </View>
     );
   }
@@ -29,11 +31,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: Platform.OS === 'android' || platformVersion < 11
+      ? Constants.statusBarHeight
+      : 30,
   },
   feed: {
     flex: 1,
-    marginTop: Platform.OS === 'android' || platformVersion < 11
-      ? Constants.statusBarHeight
-      : 0,
   }
 });
---
+++ components/CommentList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+export default class CommentList extends React.Component {
+  static propTypes = {
+    items: PropTypes.arrayOf(PropTypes.string).isRequired
+  }
+
+  renderItem = (item) => 
+    <View style={styles.itemContainer}>
+      <Text>{item}</Text>
+    </View>
+
+  render() {
+    const {items} = this.props;
+    return (
+      <ScrollView style={styles.container}>
+        {items.map(this.renderItem)}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    marginLeft: 20,
+    paddingVertical: 20,
+    paddingRight: 20,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#e0e0e0'
+  }
+})
